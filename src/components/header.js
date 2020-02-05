0b2925a81e608ee97aa377b99b40d8646a5a66b8
avidrutham <divyamathur89@gmail.com>
2020-02-05 17:01:55 +0530

    finish header styles

--- src/components/header.js
+++ src/components/header.js
@@ -1,31 +1,82 @@
 import React from "react"
 import { Link } from "gatsby"
+import headerStyles from "./header.module.scss"
 
-const Header = () => {
-  return (
-    <header>
-      <div>
-        <h1>
-          <Link to="/">Codernewbee</Link>
-        </h1>
-      </div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <a href="https://luckyrose89.github.io/portfolio-codernewbee/">
-              Portfolio
-            </a>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  )
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      visibility: false,
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(e) {
+    e.preventDefault()
+    if (!this.state.visibility) {
+      this.setState({
+        visibility: true,
+      })
+    } else {
+      this.setState({
+        visibility: false,
+      })
+    }
+  }
+
+  render() {
+    return (
+      <header className={headerStyles.header}>
+        <div className={headerStyles.headerContainer}>
+          <div>
+            <h1 className={headerStyles.brand}>
+              <Link to="/">Codernewbee</Link>
+            </h1>
+          </div>
+          <nav className={headerStyles.main_nav}>
+            <ul className={headerStyles.items}>
+              <li className={headerStyles.item}>
+                <Link to="/about">About</Link>
+              </li>
+              <li className={headerStyles.item}>
+                <a href="https://luckyrose89.github.io/portfolio-codernewbee/">
+                  Portfolio
+                </a>
+              </li>
+              <li className={headerStyles.item}>
+                <Link to="/contact">Contact</Link>
+              </li>
+            </ul>
+          </nav>
+          <button
+            className={headerStyles.toggleButton}
+            onClick={this.handleClick}
+          >
+            <span className={headerStyles.toggleButton__bar}></span>
+            <span className={headerStyles.toggleButton__bar}></span>
+            <span className={headerStyles.toggleButton__bar}></span>
+          </button>
+        </div>
+        {this.state.visibility ? (
+          <div className={headerStyles.mobileNav}>
+            <ul className={headerStyles.mobileItems}>
+              <li className={headerStyles.mobileItem}>
+                <Link to="/about">About</Link>
+              </li>
+              <li className={headerStyles.mobileItem}>
+                <a href="https://luckyrose89.github.io/portfolio-codernewbee/">
+                  Portfolio
+                </a>
+              </li>
+              <li className={headerStyles.mobileItem}>
+                <Link to="/contact">Contact</Link>
+              </li>
+            </ul>
+          </div>
+        ) : null}
+      </header>
+    )
+  }
 }
 
 export default Header
--- src/components/layout.js
+++ src/components/layout.js
@@ -2,12 +2,16 @@ import React from "react"
 
 import Header from "./header"
 import Footer from "./footer"
+import "../styles/index.scss"
+import layoutStyles from "./layout.module.scss"
 
 const Layout = props => {
   return (
-    <div>
-      <Header />
-      {props.children}
+    <div className={layoutStyles.wrapper}>
+      <div className={layoutStyles.content}>
+        <Header />
+        <div className={layoutStyles.container}>{props.children}</div>
+      </div>
       <Footer />
     </div>
   )
