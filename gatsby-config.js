896f49ef05e99b9f2ab436e6e9750ca7b1220cef
avidrutham <divyamathur89@gmail.com>
2020-02-08 16:23:16 +0530

    add dotenv plugin for env variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,13 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
+// Initialize dotenv
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`, // or '.env'
+})
+
+// And then you can use the config in gatsby-config.js
+const config = require("gatsby-plugin-config").default
 
 module.exports = {
   siteMetadata: {
